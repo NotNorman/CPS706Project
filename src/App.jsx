35e5327f3c6a0a8bbf17b0be796c6c48d6693e57
norman <chief4killin@gmail.com>
2023-04-10 18:16:18 -0400

    init

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import NetworkGraph from "./NetworkGraph";
+
+
+function App() {
+  return (
+    <div>
+     
+      <NetworkGraph></NetworkGraph>
+    </div>
+  );
+}
+export default App;
---
+++ src/NetworkGraph.jsx
@@ -0,0 +1,465 @@
+import React, { useState, useEffect } from "react";
+import { Network } from "vis-network";
+
+function NetworkGraph() {
+  const [network, setNetwork] = useState(null);
+  const [nodeList, setNodeList] = useState([]);
+  const [edgeList, setEdgeList] = useState([]);
+
+  const [start, setStart] = useState([]);
+  let output = [];
+  const [stringArray, setStringArray] = useState([]);
+
+  const stringDisplay = () => {
+    setStringArray(output);
+  }
+
+  const addNode = () => {
+    const nodeName = prompt("Enter node name:");
+    if (nodeName) {
+      const nodeExists = nodeList.some((node) => node.id === nodeName);
+      if (!nodeExists) {
+        setNodeList((prevList) => [...prevList, { id: nodeName, label: nodeName }]);
+      } else {
+        alert("Node already exists.");
+      }
+    }
+  };
+
+
+  const addEdge = () => {
+    const fromNode = prompt("Enter starting node for edge:");
+    const toNode = prompt("Enter ending node for edge:");
+    const weight = prompt("Enter edge weight:");
+  
+    // check if both fromNode and toNode exist in nodeList
+    const fromNodeExists = nodeList.some((node) => node.id === fromNode);
+    const toNodeExists = nodeList.some((node) => node.id === toNode);
+  
+    if (fromNodeExists && toNodeExists && weight) {
+      // check if an edge already exists between the given nodes
+      const edgeExists = edgeList.some(
+        (edge) => edge.from === fromNode && edge.to === toNode
+      );
+  
+      if (!edgeExists) {
+        setEdgeList((prevList) => [
+          ...prevList,
+          { from: fromNode, to: toNode, label: weight },
+        ]);
+      } else {
+        // edge already exists, show an error message or handle the case in some way
+        alert("Edge already exists between the given nodes.");
+      }
+    } else {
+      // either fromNode or toNode does not exist in the nodeList, show an error message or handle the case in some way
+      alert("One or both of the nodes do not exist.");
+    }
+  };
+
+  const deleteEdge = () => {
+    const fromNode = prompt("Enter starting node for edge:");
+    const toNode = prompt("Enter ending node for edge:");
+  
+    // check if an edge already exists between the given nodes
+    const edgeExists = edgeList.some(
+      (edge) =>
+        (edge.from === fromNode && edge.to === toNode) ||
+        (edge.from === toNode && edge.to === fromNode)
+    );
+  
+    if (edgeExists) {
+      // delete edges in both directions
+      setEdgeList((prevList) =>
+        prevList.filter(
+          (edge) =>
+            !(
+              (edge.from === fromNode && edge.to === toNode) ||
+              (edge.from === toNode && edge.to === fromNode)
+            )
+        )
+      );
+    } else {
+      // edge does not exist, show an error message or handle the case in some way
+      alert("Edge does not exist between the given nodes.");
+    }
+  };
+
+  useEffect(() => {
+    drawGraph();
+  }, [nodeList, edgeList, start]);
+
+  const loadDefaultGraph = () => {
+    setNodeList([
+      { id: "U", label: "U" },
+      { id: "X", label: "X" },
+      { id: "V", label: "V" },
+      { id: "W", label: "W" },
+      { id: "Y", label: "Y" },
+      { id: "Z", label: "Z" },
+    ]);
+    setEdgeList([
+      { from: "U", to: "V" , label: "2"},
+      { from: "U", to: "W" , label: "5"},
+      { from: "U", to: "X" , label: "1"},
+      { from: "V", to: "X" , label: "2"},
+      { from: "V", to: "W" , label: "3"},
+      { from: "W", to: "Y" , label: "1"},
+      { from: "W", to: "Z" , label: "5"},
+      { from: "W", to: "X" , label: "3"},
+      { from: "X", to: "Y" , label: "1"},
+      { from: "Y", to: "Z" , label: "2"},
+    ]);
+  };
+
+  const getNodeEdges = (nodeList, edgeList) => {
+    const edgesByNode = {};
+  
+    // create an array to store the ids of all nodes
+    const nodeIds = nodeList.map((node) => node.id);
+  
+    // iterate through each node id and get its edges
+    nodeIds.forEach((nodeId) => {
+      // find all edges where this node is the "from" node
+      const fromEdges = edgeList.filter((edge) => edge.from === nodeId);
+      // find all edges where this node is the "to" node
+      const toEdges = edgeList.filter((edge) => edge.to === nodeId);
+  
+      // create an array to store this node's edges and weights
+      const edgesWithWeights = [];
+  
+      // add each "from" edge to the array, with its weight
+      fromEdges.forEach((edge) => {
+        edgesWithWeights.push({ node: edge.to, weight: edge.label });
+      });
+  
+      // add each "to" edge to the array, with its weight
+      toEdges.forEach((edge) => {
+        edgesWithWeights.push({ node: edge.from, weight: edge.label });
+      });
+  
+      // add this node's edges and weights to the object
+      edgesByNode[nodeId] = edgesWithWeights;
+    });
+  
+    return edgesByNode;
+  };
+
+  const outputInitdij = () => {
+    output = [];
+    let tempStr = "";
+    const nodeIds = nodeList.map((node) => node.id);
+    tempStr = tempStr + "Step \t\t N'\t\t"
+     for (let i in nodeIds) {
+      const nodeId = nodeIds[i];
+      if (nodeId != start){
+      tempStr = tempStr + `D(${nodeId})p(${nodeId})\t\t`;}
+      
+     }
+     output.push(tempStr);
+  }
+
+  const calcWeight = (dijEdges, curNode) => {
+    let acc = Number(curNode["weight"]);
+    let traverseNode = curNode["node"];
+    while (traverseNode != start) {
+      acc += Number(dijEdges[traverseNode]["weight"]);
+      traverseNode = dijEdges[traverseNode]["node"];
+    }
+    return acc;
+  }
+
+  const outputFormater = (distances, visisted, dijEdges, counter) => {
+    let tempStr = `${counter}\t\t`;
+    const nodeIds = nodeList.map((node) => node.id);
+    const visit = Object.keys(visisted); 
+    for (let i in visit){
+      tempStr = tempStr + visit[i];
+    }
+    tempStr += "\t\t"
+    for (let i in nodeIds) {
+      const nodeId = nodeIds[i];
+      if (!visit.includes(nodeId)) {
+        if (distances[nodeId] === Infinity ){
+          tempStr = tempStr + "Inf\t\t\t";
+          
+        }
+        else{
+          if (dijEdges.hasOwnProperty(nodeId)){
+            tempStr = tempStr + dijEdges[nodeId]["node"] + ","+ calcWeight(dijEdges, dijEdges[nodeId]) + "\t\t\t";
+          }
+        }
+      }
+      else if (nodeId != start){
+        tempStr += "\t\t\t";
+      }
+  }
+    output.push(tempStr);
+  }
+
+  const dijkstraCaller = () => {
+    
+    if (nodeList.some(node => node.id === start)) {
+      dijkstra();
+    }
+    else{
+      const container = document.getElementById("algorithmGraph");
+      container.innerHTML = "<p>Error: Please Select Initial Vertex</p>";
+    }
+  }
+
+  const bellmanCaller = () => {
+    if (nodeList.some(node => node.id === start)) {
+      bellmanFord();
+    }
+    else{
+      const container = document.getElementById("algorithmGraph");
+      container.innerHTML = "<p>Error: Please Select Initial Vertex</p>";
+    }
+  }
+  const dijkstra = () =>{
+  
+    const graph = getNodeEdges(nodeList, edgeList);
+    
+    const distances = {};
+    const visisted = {};
+    const dijEdges = {};
+    let unvisited = Object.keys(graph);
+    let arr = [];
+
+    unvisited.forEach((node) => {
+      distances[node] = Infinity;
+    });
+
+    outputInitdij();
+    let counter = 0;
+
+    distances[start] = 0;
+
+    while (unvisited.length > 0){
+      let currentNode = null;
+      let tempEdge = {};
+      let curLeastDistance = Infinity;
+      unvisited.forEach((node) => {
+        if (currentNode === null || distances[node] < distances[currentNode]) {
+          currentNode = node;
+        }
+      });
+
+      const neighbors = graph[currentNode];
+
+      neighbors.forEach((neighbor) => {
+        let distance = Number(neighbor.weight);
+        let newDistance = distances[currentNode] + distance;
+        
+        if (newDistance < distances[neighbor.node]){
+          distances[neighbor.node] = newDistance;
+          dijEdges[neighbor.node] = {node: currentNode, weight: neighbor.weight};
+        }
+        })
+      
+      visisted[currentNode] = distances[currentNode];
+      unvisited = unvisited.filter((node) => node !== currentNode);
+      outputFormater(distances, visisted, dijEdges, counter);
+      counter++;
+
+      curLeastDistance = null;
+      unvisited.forEach((node) => {
+        
+        if (curLeastDistance === null || distances[node] < distances[curLeastDistance]) {
+          curLeastDistance = node;
+        }
+      });
+      if (dijEdges.hasOwnProperty(curLeastDistance)){
+        arr.push({ from: curLeastDistance, to: dijEdges[curLeastDistance].node, label: dijEdges[curLeastDistance].weight })
+      }
+    }
+    
+    output.map((str) => console.log(str));
+    stringDisplay();
+    const data = {
+      nodes: nodeList,
+      edges: arr,
+    };
+    const options = {
+      edges: {
+        labelHighlightBold: false,
+        font: { size: 20, color: '#000000' },
+      },
+      nodes: {
+        font: { size: 20, color: '#000000' },
+      },layout: {
+        hierarchical: false,
+        randomSeed: 2,
+      },
+    };
+    const container = document.getElementById("algorithmGraph");
+    const network = new Network(container, data, options);
+    network.body.nodes[start].setOptions({ color: { background: "red" } });
+    setNetwork(network);
+  }
+
+  const startVertex = () => {
+    const tempStart = prompt("Enter initial node by name:");
+    const nodeExists = nodeList.some((node) => node.id === tempStart);
+    
+    if (nodeExists) {
+      setStart(tempStart);
+    } else {
+      alert("The entered node does not exist.");
+    }
+  };
+
+  const outputAppendBell = (dist, counter) => {
+    console.log(dist);
+    output.push("Step: " + counter);
+    output.push(JSON.stringify(dist));
+    // for (const key in dist) {
+    //   output.push(`${key}: ${dist[key]}`);
+    // }
+    return counter+=1;
+  }
+
+  const bellmanFord = () => {
+    const distances = {};
+    const prevNodes = {};
+    const nodeIds = nodeList.map((node) => node.id);
+    const bestEdge = {};
+    let counter = 0;
+  
+    // Initialize all distances to Infinity and prevNodes to null
+    for (let i in nodeIds) {
+      const nodeId = nodeIds[i];
+      distances[nodeId] = Infinity;
+      prevNodes[nodeId] = null;
+    }
+  
+    // Set distance of start node to 0
+    distances[start] = 0;
+  
+    counter = outputAppendBell(distances, counter);
+    
+    // Relax edges |V|-1 times
+    for (let i = 0; i < nodeIds.length - 1; i++) {
+       // Forward direction
+    for (let j = 0; j < edgeList.length; j++) {
+      const edge = edgeList[j];
+      let fromNode = edge.from;
+      let toNode = edge.to;
+      const weight = Number(edge.label);
+      let newDistance = distances[fromNode] + weight;
+
+      if (newDistance < distances[toNode]) {
+        bestEdge[toNode] = { from: fromNode, to: toNode, label: String(weight) };
+        distances[toNode] = newDistance;
+        prevNodes[toNode] = fromNode;
+      }
+
+      // Backward direction
+      fromNode = edge.to;
+      toNode = edge.from;
+      newDistance = distances[fromNode] + weight;
+      if (newDistance < distances[toNode]) {
+            bestEdge[toNode] = { from: fromNode, to: toNode, label: String(weight) };
+            distances[toNode] = newDistance;
+            prevNodes[toNode] = fromNode;
+          }
+    }
+
+    counter = outputAppendBell(distances, counter);
+  }
+  
+    // Check for negative-weight cycles
+    for (let i in edgeList) {
+      const fromNode = edgeList[i].from;
+      const toNode = edgeList[i].to;
+      const weight = Number(edgeList[i].label);
+  
+      if (isFinite(distances[toNode]) && distances[fromNode] + weight < distances[toNode]) {
+        console.log(distances[fromNode] + weight, distances[toNode]);
+        throw new Error("Negative-weight cycle detected");
+      }
+    }
+    const data = {
+      nodes: nodeList,
+      edges: Object.values(bestEdge),
+    };
+    const options = {
+      edges: {
+        labelHighlightBold: false,
+        font: { size: 20, color: '#000000' },
+      },
+      nodes: {
+        font: { size: 20, color: '#000000' },
+      },layout: {
+        hierarchical: false,
+        randomSeed: 2,
+      },
+    };
+    const container = document.getElementById("algorithmGraph");
+    networkConstruct(container, data, options);
+    stringDisplay();
+  };
+
+  const networkConstruct = (container, data, options) => {
+    if (nodeList.some(node => node.id === start)) {
+      const network = new Network(container, data, options);
+      network.body.nodes[start].setOptions({ color: { background: "red" } });
+      setNetwork(network);
+    }
+    else{
+      const network = new Network(container, data, options);
+      setNetwork(network);
+    }
+  }
+  
+
+  const drawGraph = () => {
+    const data = {
+      nodes: nodeList,
+      edges: edgeList,
+    };
+    const options = {
+      edges: {
+        labelHighlightBold: false,
+        font: { size: 20, color: '#000000' },
+      },
+      nodes: {
+        font: { size: 20, color: '#000000' },
+      },layout: {
+        hierarchical: false,
+        randomSeed: 2,
+      },
+    };
+    const container = document.getElementById("network");
+    networkConstruct(container, data, options);
+  };
+
+  return (
+    <div>
+      <button onClick={addNode}>Add Node</button>
+      <button onClick={addEdge}>Add Edge</button>
+      <button onClick={deleteEdge}>Delete Edge</button>
+      <button onClick={loadDefaultGraph}>Default</button>
+      <button onClick={drawGraph}>Draw Graph</button>
+      <button onClick={startVertex}>Choose Initial Vertex</button>
+      <button onClick={dijkstraCaller}>Dijkstra</button>
+      <button onClick={bellmanCaller}>DV: Bellman-Ford</button>
+      <div style={{display: "flex"}}>
+      <div style={{ width: "50%" }}>
+        <p>Original graph:</p>
+        <div id="network" style={{ width: "100%", height: "500px" }}></div>
+      </div>
+      <div style={{ width: "50%" }}>
+        <p>Algorithm Applied:</p>
+        <div id="algorithmGraph" style={{ width: "100%", height: "500px" }}></div>
+      </div>
+    </div>
+      <div id="output">{stringArray.map((string, index) => (
+          <pre key={index}>{string}</pre>
+        ))}</div>
+    </div>
+  );
+}
+
+export default NetworkGraph;
